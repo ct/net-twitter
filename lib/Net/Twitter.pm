34ba06290739b50aa26a61f7a86baab6b48d4c38
Chris Thompson <chris@cthompson.com>
2009-04-07 13:10:07 -0400

    Screw it, let's go for 3.00

--- lib/Net/Twitter.pm
+++ lib/Net/Twitter.pm
@@ -1,11 +1,11 @@
 ##############################################################################
 # Net::Twitter - Perl OO interface to www.twitter.com
-# v2.13
+# v3.00
 # Copyright (c) 2009 Chris Thompson
 ##############################################################################
 
 package Net::Twitter;
-$VERSION = "2.13";
+$VERSION = "3.00";
 use 5.005;
 use strict;
 
@@ -609,7 +609,7 @@ Net::Twitter - Perl interface to twitter.com
 
 =head1 VERSION
 
-This document describes Net::Twitter version 2.13
+This document describes Net::Twitter version 3.00
 
 =head1 SYNOPSIS
 
